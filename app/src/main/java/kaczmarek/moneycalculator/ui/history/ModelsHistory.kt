a1ec934e29cd6f7eab63f75b5ddb9b54d00d98ee
Angelina Podbolotova <a.podbolotova@mintmail.ru>
2022-03-28 22:17:48 +0300

    removed all project and compose dependency

--- app/src/main/java/kaczmarek/moneycalculator/ui/history/ModelsHistory.kt
+++
@@ -1,8 +0,0 @@
-package kaczmarek.moneycalculator.ui.history
-
-import kaczmarek.moneycalculator.di.services.database.models.Session
-import kaczmarek.moneycalculator.ui.base.BaseItem
-
-class DateItem(val date: String) : BaseItem
-
-class SessionItem(val session: Session, var isShow: Boolean) : BaseItem
