0819d8351e88bbed720bc5421c8246ae54c2abd1
Angelina Podbolotova <developer.kaczmarek@yandex.ru>
2022-04-25 21:22:21 +0300

    fixed database

--- app/src/main/kotlin/kaczmarek/moneycalculator/core/banknote/data/BanknoteDbModel.kt
+++ app/src/main/kotlin/kaczmarek/moneycalculator/core/banknote/data/BanknoteDbModel.kt
@@ -5,33 +5,48 @@ import androidx.room.PrimaryKey
 import kaczmarek.moneycalculator.core.banknote.domain.Banknote
 import kaczmarek.moneycalculator.core.banknote.domain.BanknoteId
 import kaczmarek.moneycalculator.core.banknote.domain.DetailedBanknote
+import kaczmarek.moneycalculator.core.utils.toFormattedAmount
 import kotlinx.serialization.Serializable
 
 @Serializable
 @Entity(tableName = "banknotes")
 data class BanknoteDbModel(
-    val name: String,
+    val name: Double,
+    val count: Int,
+    val amount: Double,
     val backgroundColor: String,
-    val value: Float,
+    val textColor: String,
     val isShow: Boolean
 ) {
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0
 }
 
-fun BanknoteDbModel.toBanknote() = Banknote(
-    id = BanknoteId(id.toString()),
-    name = name,
-    isShow = isShow,
-    denomination = value.toDouble()
-)
+fun BanknoteDbModel.toBanknote(): Banknote {
+    val denomination = name
+    val formattedName =
+        (if (denomination < 1) denomination * 100 else denomination).toFormattedAmount()
 
-fun BanknoteDbModel.toDetailedBanknote() = DetailedBanknote(
-    id = BanknoteId(id.toString()),
-    name = name,
-    isShow = isShow,
-    denomination = value.toDouble(),
-    backgroundColor = ("ff" + backgroundColor.removePrefix("#").lowercase()).toLong(16),
-    count = "0",
-    amount = "0"
-)
+    return Banknote(
+        id = BanknoteId(id.toString()),
+        name = formattedName,
+        isShow = isShow,
+        denomination = denomination
+    )
+}
+
+fun BanknoteDbModel.toDetailedBanknote(): DetailedBanknote {
+    val denomination = name
+    val formattedName =
+        (if (denomination < 1) denomination * 100 else denomination).toFormattedAmount()
+
+    return DetailedBanknote(
+        id = BanknoteId(id.toString()),
+        name = formattedName,
+        isShow = isShow,
+        denomination = denomination,
+        backgroundColor = ("ff" + backgroundColor.removePrefix("#").lowercase()).toLong(16),
+        count = "0",
+        amount = "0"
+    )
+}
